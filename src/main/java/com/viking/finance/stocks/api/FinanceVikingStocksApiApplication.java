060804e135c915e597fe467a75216d714a39d727
Jacob Morgan <jacobmorgan1989@gmail.com>
2021-10-26 17:08:39 -0500

    Initial commit

---
+++ src/main/java/com/viking/finance/stocks/api/FinanceVikingStocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.viking.finance.stocks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceVikingStocksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceVikingStocksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viking/finance/stocks/api/FinanceVikingStocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viking.finance.stocks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceVikingStocksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
